3cd0013ec616a372acffbbef1a6a02fe27e224c9
P-1000 <pavan.patchikarla@yahoo.com>
2023-09-09 06:26:23 +0530

    new fix

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const fs = require('fs');
-const path = require('path');
-const libre = require('libreoffice-convert');
+#!/usr/bin/env node
+import fs from 'fs';
+import path from 'path';
+import libre from 'libreoffice-convert';
+import figlet from 'figlet';
+import chalk from 'chalk';
+
+// Function to ensure that a directory exists
+function ensureDirectoryExists(directory) {
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory, { recursive: true });
+  }
+}
 
 // Function to convert an Office file (e.g., doc, docx, ppt, pptx) to PDF
 function convertOfficeToPdf(inputFile, outputDirectory) {
@@ -11,45 +21,50 @@ function convertOfficeToPdf(inputFile, outputDirectory) {
 
   libre.convert(officeFileBuffer, '.pdf', undefined, (err, pdfBuffer) => {
     if (err) {
-      console.log(`Error converting file ${inputFile}: ${err}`);
+      console.error(chalk.red(`Error converting file ${inputFile}: ${err}`));
     } else {
+      ensureDirectoryExists(outputDirectory); // Ensure output directory exists
       fs.writeFileSync(outputPdfFile, pdfBuffer);
-      console.log(`File converted successfully: ${inputFile} -> ${outputPdfFile}`);
+      console.log(chalk.green.bold(`File converted successfully: ${inputFile} -> ${outputPdfFile}`));
     }
   });
 }
 
 // Process command-line arguments
 if (process.argv.length !== 4) {
-  console.error('Usage: node convert.js input-folder output-subdirectory');
+  console.error(chalk.red('Usage: node convert.js input-folder output-subdirectory'));
   process.exit(1);
 }
 
 const inputFolder = process.argv[2];
-const outputSubdirectory = process.argv[3]; // Specify the subdirectory name
+const outputSubdirectory = process.argv[3];
 
 // Define the full output directory path
 const outputDirectory = path.join('output', outputSubdirectory);
 
-// Create the output directory if it doesn't exist
-if (!fs.existsSync(outputDirectory)) {
-  fs.mkdirSync(outputDirectory, { recursive: true });
-}
-
-// Read all files in the input directory and convert Office files to PDF
-fs.readdir(inputFolder, (err, files) => {
+// Create a cool banner
+figlet('PPT-PDF', (err, data) => {
   if (err) {
-    console.error(`Error reading input folder: ${err}`);
-    process.exit(1);
+    console.error(chalk.red('Error creating banner:', err));
+  } else {
+    console.log(chalk.blue(data));
   }
 
-  files.forEach((file) => {
-    const inputFile = path.join(inputFolder, file);
-    const fileExtension = path.extname(file).toLowerCase();
-
-    // Check for supported Office file extensions and exclude PDFs
-    if (['.doc', '.docx', '.ppt', '.pptx'].includes(fileExtension) && fileExtension !== '.pdf') {
-      convertOfficeToPdf(inputFile, outputDirectory);
+  // Read all files in the input directory and convert Office files to PDF
+  fs.readdir(inputFolder, (err, files) => {
+    if (err) {
+      console.error(chalk.red(`Error reading input folder: ${err}`));
+      process.exit(1);
     }
+
+    files.forEach((file) => {
+      const inputFile = path.join(inputFolder, file);
+      const fileExtension = path.extname(file).toLowerCase();
+
+      // Check for supported Office file extensions and exclude PDFs
+      if (['.doc', '.docx', '.ppt', '.pptx'].includes(fileExtension) && fileExtension !== '.pdf') {
+        convertOfficeToPdf(inputFile, outputDirectory);
+      }
+    });
   });
 });
